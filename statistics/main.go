8aa854f0f1e763483aebc5ac46755492c7eb4e84
mzki <28139969+mzki@users.noreply.github.com>
2017-05-15 20:01:44 +0900

    statistics: collect information for question segment

--- statistics/main.go
+++ statistics/main.go
@@ -1,3 +1,5 @@
+//go:generate go run main.go
+//
 // +build stat
 
 package main
@@ -5,33 +7,73 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/mzki/feserver/src"
 )
 
+const File = "./statistics.csv"
+
 func main() {
+	fp, err := os.Create(File)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	writeStatistics(fp)
+}
+
+func writeStatistics(writer io.Writer) {
+	fmt.Fprintf(writer, "year, season, imageCount, rate, techRate, manageRate, strategyRate, errCount\n")
+
 	totalCount := 0
+	totalTech := 0
+	totalMana := 0
+	totalStrat := 0
 	cycle := 0
+
 	for y := src.MinYear; y <= src.MaxYear; y++ {
 		for _, s := range []string{src.SeasonSpring, src.SeasonAutumn} {
-			count, errCount := countHasImage(y, s)
+			count, techC, manaC, stratC, errCount := countHasImage(y, s)
+
 			totalCount += count
+			totalTech += techC
+			totalMana += manaC
+			totalStrat += stratC
 			cycle += 1
-			fmt.Fprintf(os.Stdout, "year:%d, season:%s, imageCount:%d/%d, rate:%.2f, errCount:%d\n",
-				y, s, count, src.MaxNo, float64(count)/float64(src.MaxNo), errCount)
+
+			fmt.Fprintf(writer, "%d, %s, %d/%d, %.2f, %.2f, %.2f, %.2f, %d\n",
+				y, s, count, src.MaxNo, rate(count), rate(techC), rate(manaC), rate(stratC), errCount)
 		}
 	}
-	fmt.Fprintf(os.Stdout, "# total, imageCount:%d/%d, rate:%.2f\n",
-		totalCount, cycle*src.MaxNo, float64(totalCount)/float64(cycle*src.MaxNo))
+
+	totalQ := cycle * src.MaxNo
+	fmt.Fprintf(writer, "# total, imageCount, rate, techRate, manageRate, strategyRate\n")
+	fmt.Fprintf(writer, "# - , %d/%d, %.2f, %.2f, %.2f, %.2f\n",
+		totalCount, totalQ,
+		float64(totalCount)/float64(totalQ),
+		float64(totalTech)/float64(totalQ),
+		float64(totalMana)/float64(totalQ),
+		float64(totalStrat)/float64(totalQ),
+	)
 }
+
+func rate(a int) float64 {
+	return float64(a) / float64(src.MaxNo)
+}
+
+const (
+	TechMax  = 50
+	ManaMax  = 60
+	StratMax = 80
+)
 
 var getter = src.NewGetter(src.LeastIntervalTime)
 
-func countHasImage(year int, season string) (int, int) {
+func countHasImage(year int, season string) (count, techC, manaC, stratC, errCount int) {
 	ctx := context.Background()
-	count := 0
-	errCount := 0
 	for q := src.MinNo; q <= src.MaxNo; q++ {
 		res, err := getter.Get(ctx, src.Query{
 			Year:   year,
@@ -45,7 +87,15 @@ func countHasImage(year int, season string) (int, int) {
 		}
 		if res.HasImage {
 			count++
+			switch {
+			case q <= TechMax:
+				techC += 1
+			case q <= ManaMax:
+				manaC += 1
+			case q <= StratMax:
+				manaC += 1
+			}
 		}
 	}
-	return count, errCount
+	return
 }
